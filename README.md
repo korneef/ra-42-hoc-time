6fe5de323c1e22512e4060c01b7e24881d4ad554
Valentin Korneev <v.korneef@gmail.com>
2022-12-19 15:10:46 +0600

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build Status](https://api.cirrus-ci.com/github/korneef/ra-42-hoc-time.svg)](https://cirrus-ci.com/github/korneef/ra-42-hoc-time)
+
+Ссылка на проект https://korneef.github.io/ra-42-hoc-time
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { MainPage } from './components/index';
+import AppReact from './components/App';
+
+function App() {
+  return (
+    <div className="App">
+      <AppReact/>
+    </div>
+  );
+}
+
+export default App;
